8bd0b725f6bf5c879a7a9b1bbcc8bb17d7fb620a
Luis Dandolini <luisfernandodandoliniduarte@gmail.com>
2023-01-06 17:14:25 -0300

    add day 1

---
+++ basics/triangle/main.js
@@ -0,0 +1,9 @@
+/*
+4. Write a JavaScript program to find the area of a triangle where lengths of the three of its sides are 5, 6, 7.
+*/
+
+function triangleLength(base1, base2, base3) {
+  return (base1 + base2 + base3) / 2
+}
+
+console.log(triangleLength(5, 6, 7))
